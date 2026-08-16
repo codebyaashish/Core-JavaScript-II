d4be57445930314515f6589dd9d94c45557f19e0
Aashish Vidyarthi <aashish301v@gmail.com>
2026-08-16 09:25:16 +0530

    Event bubbling

---
+++ MiniProj/eventbunnling.js
@@ -0,0 +1,21 @@
+let div=document.querySelector("div");
+let ul=document.querySelector("ul");
+let lis=document.querySelectorAll("li");
+
+
+
+div.addEventListener("click",function(){
+    console.log("div was clicked");
+})
+//event bubbling
+ul.addEventListener("click",function(event){
+    event.stopPropagation();
+    console.log("ul was clicked");
+})
+//eventbubbling
+for(li of lis){
+    li.addEventListener('click',function(event){
+        event.stopPropagation();
+        console.log("li was clicked");
+    })
+}
